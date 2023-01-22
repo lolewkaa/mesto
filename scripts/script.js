c378069a7b0b503d9c51195ef4afe9b2dee71699
Anastasia <menshikova.naya@mail.ru>
2023-01-22 19:48:47 +0500

    Проект Место

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,23 @@
 let popups = document.querySelector('.popup');
-popups.classList.remove('popup_opened');
-
 
 let button = document.querySelector('.profile__button');
-button.addEventListener('click', function (){
-popups.classList.add('popup_opened');
-})
+
+
+
+function showClick() {
+  popups.classList.add('popup_opened');
+  nameInput.value = author.textContent;
+  jobInput.value = job.textContent;
+}
+
+button.addEventListener('click', showClick);
+
+function removeClick() {
+  popups.classList.remove('popup_opened');
+}
 
 let closeicon = document.querySelector('.form__button');
-closeicon.addEventListener('click', function (){
-  popups.classList.remove('popup_opened');
-})
+closeicon.addEventListener('click', removeClick);
 
 
 /*let likebutton = document.querySelector('section.cards div.card .card__button');
@@ -23,21 +30,18 @@ likebutton.classList.toggle('card__button_active');
 
 let formElement = document.querySelector('.form');
 
-let nameInput = document.querySelector('.form__user');
-let jobInput = document.querySelector('.form__item');
+let nameInput = document.querySelector('.form__item_type_name');
+let jobInput = document.querySelector('.form__item_type_job');
 
 let author = document.querySelector('.profile__user');
 let job = document.querySelector('.profile__text');
-
-let saveicon = document.querySelector('.form__button-save');
 
 function handleFormSubmit (evt) {
   evt.preventDefault(); 
   author.textContent = nameInput.value;
   job.textContent = jobInput.value;
-  saveicon.addEventListener('click', function (){
-    popups.classList.remove('popup_opened');
-  })
+  removeClick();
+  
 }
 
 formElement.addEventListener('submit', handleFormSubmit);
